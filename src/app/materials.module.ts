8a94470d8ec66330e01e504591234f31b44fdda8
zacky <E0284042@u.nus.edu>
2019-10-03 22:25:45 +0800

    test

--- src/app/materials.module.ts
+++ src/app/materials.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core'
 import {MatInputModule} from '@angular/material/input'
-import { MatButtonModule} from '@angular/material/button';
-import { MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
-const MATERIAL = [ MatInputModule, MatButtonModule, MatIconModule];
+const MATERIAL = [ MatInputModule, MatButtonModule, MatIconModule,MatDatepickerModule, MatToolbarModule];
 
 @NgModule({
 imports: MATERIAL,
